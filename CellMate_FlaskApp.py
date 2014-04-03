75bbcc4bb6ae8d4efd81b6bfa974aedffc2d26fa
Hana Snow <hanasnow@nat-oitwireless-inside-vapornet100-c-17581.princeton.edu>
2014-04-03 17:25:56 -0400

    update test

--- CellMate_FlaskApp.py
+++ CellMate_FlaskApp.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Python Flask App!"
+    return "Welcome to the *updated* Python Flask App!"
 
 if __name__ == "__main__":	
 	app.run()
