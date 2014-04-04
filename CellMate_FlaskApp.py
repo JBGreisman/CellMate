2479a92658c8c4d11c78bdcc52ab43278038163d
hanasnow <hsnow@princeton.edu>
2014-04-04 18:00:13 -0400

    db files

--- CellMate_FlaskApp.py
+++ CellMate_FlaskApp.py
@@ -1,45 +1,16 @@
 import os
 from flask import Flask
 import psycopg2
-from flask.ext.sqlalchemy import SQLAlchemy
-#from db_manager import db
+import db_manager as dbm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #db.app =  app
-db = SQLAlchemy()
-db.init_app(app)
-
-class User(db.Model):
-	__tablename__ = 'USERS'
-	uid = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(50), unique = True)
-
-	def __init__(self, name):
-		self.name = name
-
-#gets the uid associated with the username, or None if no user exists
-def get_uid(username):
-	user = User.query.filter_by(name = username).first()
-	if user:
-		return user.uid
-	else:
-		return None
-
-#creates user with the given username if it doesn't already exists
-def create_user(username):
-	#can't add duplicate users
-	if get_uid(username):
-		return False
-	db.session.add(User(username))
-	db.session.commit()
-	return True
-
+dbm.db.init_app(app)
 
 @app.route("/")
 def hello():
-    return str(get_uid('Hana'))
-    #return "CellMate"
+    return "Hello, Hana. Your id is " + str(dbm.get_uid('Hana'))
 
 if __name__ == "__main__":
 	app.run()
--- db_manager.py
+++ db_manager.py
@@ -1,11 +1,28 @@
 from flask.ext.sqlalchemy import SQLAlchemy
  
-#db = SQLAlchemy()
+db = SQLAlchemy()
  
 class User(db.Model):
-  __tablename__ = 'USERS'
-  uid = db.Column(db.Integer, primary_key = True, auto_increment = True)
-  name = db.Column(db.String(50), unique = True)
-   
-  def __init__(self, name):
-    self.name = name
+	__tablename__ = 'USERS'
+	uid = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(50), unique = True)
+
+	def __init__(self, name):
+		self.name = name
+
+#gets the uid associated with the username, or None if no user exists
+def get_uid(username):
+	user = User.query.filter_by(name = username).first()
+	if user:
+		return user.uid
+	else:
+		return None
+
+#creates user with the given username if it doesn't already exists
+def create_user(username):
+	#can't add duplicate users
+	if get_uid(username):
+		return False
+	db.session.add(User(username))
+	db.session.commit()
+	return True
