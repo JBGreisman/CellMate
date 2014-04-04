cc801977ae3b858b855e226665926e10199baccb
hanasnow <hsnow@princeton.edu>
2014-04-04 02:10:46 -0400

    database?

--- CellMate_FlaskApp.py
+++ CellMate_FlaskApp.py
@@ -1,22 +1,12 @@
 import os
 from flask import Flask
-#from db_manager import db, User
-
-def create_app():
-	app = Flask(__name__)
-	#loads the db connection string from a file and initializes the db connection
-	#app.config['SQLALCHEMY_DATABASE_URI'] = open('connection.txt', 'r').readline()
-	#db.init_app(app)
-	#db.app = app
-	#return app
-
-#app = create_app()
-#print 'app initialized'
-
-#print db.session.query('1').from_statement('SELECT 1').all()
-#print User.query.first()
+import psycopg2
+from flask.ext.sqlalchemy import SQLAlchemy
+from db_manager import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db.app =  app
 
 @app.route("/")
 def hello():
--- db_manager.py
+++ db_manager.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
  
 class User(db.Model):
   __tablename__ = 'USERS'
-  uid = db.Column(db.Integer, primary_key = True)
+  uid = db.Column(db.Integer, primary_key = True, auto_increment = True)
   name = db.Column(db.String(50), unique = True)
    
   def __init__(self, name):
