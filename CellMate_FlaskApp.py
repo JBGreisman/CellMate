641ec456a02c7f2b04a10e7d2a65671e9ba375e5
hanasnow <hsnow@princeton.edu>
2014-04-04 02:15:59 -0400

    database?

--- CellMate_FlaskApp.py
+++ CellMate_FlaskApp.py
@@ -7,6 +7,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #db.app =  app
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
