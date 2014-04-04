a00930df4d0914b0d585eda5b2cc6806eba8956a
hanasnow <hsnow@princeton.edu>
2014-04-04 02:18:47 -0400

    database?

--- CellMate_FlaskApp.py
+++ CellMate_FlaskApp.py
@@ -4,6 +4,14 @@ import psycopg2
 from flask.ext.sqlalchemy import SQLAlchemy
 #from db_manager import db
 
+class User(db.Model):
+  __tablename__ = 'USERS'
+  uid = db.Column(db.Integer, primary_key = True, auto_increment = True)
+  name = db.Column(db.String(50), unique = True)
+   
+  def __init__(self, name):
+    self.name = name
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #db.app =  app
@@ -11,7 +19,7 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
-    return "Welcome to CellMate! (database not crashing thae app!)"
+    return "Welcome to CellMate!"
 
 if __name__ == "__main__":	
 	app.run()
