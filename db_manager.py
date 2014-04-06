c0b94944a660b94a9d711898fcc2bbd32ceaf860
hanasnow <hsnow@princeton.edu>
2014-04-06 14:23:26 -0400

    data table

--- db_manager.py
+++ db_manager.py
@@ -28,3 +28,24 @@ class User(db.Model):
 		db.session.add(User(username))
 		db.session.commit()
 		return True
+
+class Data(db.Modlel):
+	__tablename__ = 'DATA'
+	uid = db.Column(db.Integer)
+	date = db.Column(db.Date)
+	count = db.Column(db.Integer)
+	image = db.Column(db.LargeBinary)
+	thresh_image = db.Column(db.LargeBinary)
+
+	def __init__(self, uid, date, count, image, thresh_image):
+		self.uid = uid
+		self.date = date
+		self.count = count
+		self.image = image
+		self.thresh_image = thresh_image
+
+	#gets all Data rows associated with the given uid
+	@staticmethod
+	def get_user_data(userid):
+		return Data.query.filter_by(uid = userid).all()
+
