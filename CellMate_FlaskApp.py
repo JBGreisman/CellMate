864b36beb1683a6ea134d2f6748b14ca6247c2da
hanasnow <hsnow@princeton.edu>
2014-04-29 14:52:43 -0400

    counting tests

--- CellMate_FlaskApp.py
+++ CellMate_FlaskApp.py
@@ -10,16 +10,16 @@ db.init_app(app)
 
 @app.route("/")
 def hello():
-	user = 'Hana'
-	uid = User.get_uid(user)
-	data = Data.get_user_data(uid)
+	#user = 'Hana'
+	#uid = User.get_uid(user)
+	#data = Data.get_user_data(uid)
 	#total = 0
 	#for row in data:
 	#	total = total + row.count
 
 	#return 'Hello ' + user + '! You have counted ' + str(total) + ' colonies'
 
-	img = data[0].image
+	img =  open('scrren1.png', 'r').read()
 	(count, thresh_img) = countColonies.processImage(img, 1, 1)
 	return 'colonies counted: ' + count
 
