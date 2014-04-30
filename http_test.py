d2b5aaf73410ac98cc1cc64286e43382c59ba468
hanasnow <hsnow@princeton.edu>
2014-04-30 14:16:26 -0400

    hhtp connection testing

--- http_test.py
+++ http_test.py
@@ -18,4 +18,4 @@ enc_thresh_img = response.read()
 thresh_img = base64.b64decode(enc_thresh_img)
 open('thresh_plate.png', 'wb').write(thresh_img)
 print 'threshholded image saved to thresh_plate.png, count = ' + str(count)
-print enc_thresh_img
+#print enc_thresh_img
