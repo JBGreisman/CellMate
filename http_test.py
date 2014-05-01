6093f052ec4d6effb6cc324b07631487db5c4378
hanasnow <hsnow@princeton.edu>
2014-04-30 21:20:17 -0400

    hhtp connection testing

--- http_test.py
+++ http_test.py
@@ -15,8 +15,11 @@ conn.request('GET', url_string, encoded_img)
 #get response
 response = conn.getresponse()
 resp_json = json.loads(response.read())
+
 count = int(resp_json['count'])
+
 enc_thresh_img = str(resp_json['thresh_img'])
 thresh_img = base64.b64decode(enc_thresh_img)
 open('thresh_plate.png', 'wb').write(thresh_img)
+
 print 'threshholded image saved to thresh_plate.png, count = ' + str(count)
