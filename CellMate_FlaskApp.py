93018a6e80ddbd59227c22b3b4db0e3c55a97795
hanasnow <hsnow@princeton.edu>
2014-04-30 20:15:54 -0400

    hhtp connection testing

--- CellMate_FlaskApp.py
+++ CellMate_FlaskApp.py
@@ -36,7 +36,6 @@ def count_colonies(x,y):
 	(count, thresh_img) = countColonies.processImage('img.png', x, y)
 	#cv2.imwrite('thresh_img.png', thresh_img)
 
-	count = x+y
 	enc_thresh_img = open('img.png', 'rb').read()
 	#enc_thresh_img = open('thresh_img.png', 'rb').read()
 	resp = Response(enc_thresh_img, status=count, mimetype='image/png')
